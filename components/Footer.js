baee7790784d19480647dd09d3ecba9791f33850
Emshtml <casadathereza1@gmail.com>
2025-10-26 16:45:38 -0300

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-green-700 text-white py-4 mt-8 text-center">
+      © 2025 Coco Verde Fresquinho - Todos os direitos reservados
+    </footer>
+  );
+}
